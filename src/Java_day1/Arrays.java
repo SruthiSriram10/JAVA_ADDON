ad6fdd870d0f27c27753fbdccc5257f71a60460b
Sruthi <sriramsruthi10@gmail.com>
2025-03-18 14:37:23 +0530

    Day1 & Day2

---
+++ src/Java_day1/Arrays.java
@@ -0,0 +1,16 @@
+package Java_day1;
+
+public class Arrays {
+	public static void main(String[] args) {
+		int []num=new int[5]; //single dimensional array
+		num[0]=10;
+		num[1]=20;
+		num[2]=30;
+		num[3]=40;
+		num[4]=50;
+		System.out.println(num[0]+"\n");
+		for (int i=0;i<num.length;i++) {
+			System.out.println(num[i]);
+			}
+		}
+	}
---
+++ src/Java_day1/Demo.java
@@ -0,0 +1,37 @@
+package Java_day1;
+
+
+
+
+
+public class Demo {
+
+
+
+	public static void main(String[] args) {
+
+		
+
+		System.out.println("Name: Sruthi S");
+
+		System.out.println("R.No: 2232J38");
+
+		System.out.println("Dept: Software Systems");
+
+		System.out.println("Phone: 9677463763");
+
+		System.out.println("DOB: 10.06.2004");
+
+		System.out.println("Address: Kariyampalayam");
+
+		System.out.println("Email: 2232j38@kgcas.com");
+
+		
+
+
+
+	}
+
+
+
+}
